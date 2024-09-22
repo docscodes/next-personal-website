eac7db699a244b2af6f962dc6e610f1840bdfec8
docscodes <docscodes@gmail.com>
2024-09-22 16:31:11 -0400

    fix build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,17 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import App, { AppContext, AppProps } from 'next/app'
 import Layout from '../components/layout'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
   )
 }
+
+MyApp.getInitialProps = async (context: AppContext) => {
+  const ctx = await App.getInitialProps(context);
+
+  return { ...ctx };
+};
